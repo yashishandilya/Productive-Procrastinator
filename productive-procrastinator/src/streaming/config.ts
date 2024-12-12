f3b6400784d9c6effd3d009cc1023c23349ef9b7
Yashi Shandilya <yshand2@uic.edu>
2024-12-11 20:42:59 -0600

    refresh token

--- productive-procrastinator/src/streaming/config.ts
+++ productive-procrastinator/src/streaming/config.ts
@@ -1,6 +1,6 @@
 export const options = {
     appId: "d8b3e222af6846cda5057906ced74cef",  // Your App ID
     channel: "outerspace",
-    token: "007eJxTYJCukbyq8OLu0Tvcy25V7ayYIexn9lzT2Sisnm1N2063CjYFhhSLJONUIyOjxDQzCxOz5JREUwNTc0sDs+TUFHOT5NS0G5OC0hsCGRmYV+uyMDJAIIjPxZBfWpJaVFyQmJzKwAAAzcsg1w==",   // Since we disabled authentication
+    token: "007eJxTYEj7tfaGhOmE+CczP94K5Sxc0M/mfOWD+INIr6vKvKvEXwooMKRYJBmnGhkZJaaZWZiYJackmhqYmlsamCWnppibJKemifpGpTcEMjKwb1JlYIRCEJ+LIb+0JLWouCAxOZWBAQAe9yEk",   // Since we disabled authentication
     uid: null
 };
